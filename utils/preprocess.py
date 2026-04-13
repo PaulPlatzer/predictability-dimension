fae0092a2d83d521b574e3f679dbc7230388f993
Martin Bonte <martinbo@kili.oma.be>
2026-04-13 13:49:56 +0200

    Added the spread computed with the TIGGE dataset, and compared it with the dimension

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,8 +4,6 @@ import numpy as np
 def preprocess_data(data_folder, subsampling, max_horizon):
     # load
     allpcs = xr.open_dataset(data_folder + 'pcs.nc')['pseudo_pcs']
-    eofs = xr.open_dataset(data_folder + 'eofs.nc')['eofs']
-    pourc_EOF = xr.open_dataset(data_folder + 'pourc_eofs.nc')['variance_fractions']
     
     # normalize pcs but keep relative variances
     pcs_norm = allpcs / (allpcs.sel(mode=0)).std(dim='time')
